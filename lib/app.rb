a31358cd7f57f9727cfd3030eb1e4e0b6aadb31b
Leon <leon.hewitt@crugroup.com>
2012-07-11 17:04:38 +0100

    added uris for stories and last story moved

--- lib/app.rb
+++ lib/app.rb
@@ -10,3 +10,13 @@ get '/projects' do
 	agilezen_api = Backend::AgileZenApi.new()
 	agilezen_api.get_projects
 end
+
+get '/projects/:id' do |project_id|
+	agilezen_api = Backend::AgileZenApi.new()
+	agilezen_api.get_stories_for(project_id.to_i)
+end
+
+get '/projects/:id/last_story_moved' do |project_id|
+	agilezen_api = Backend::AgileZenApi.new()
+	agilezen_api.get_last_story_moved_for(project_id.to_i)
+end
--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -7,9 +7,8 @@ describe 'Application' do
 	end
 
     def mock_api
-        agilezen_mock = double("Backend::AgileZenApi")
-        Backend::AgileZenApi.stub(:new).with().and_return(agilezen_mock)
-        agilezen_mock.should_receive(:get_projects).and_return(["project1"])
+        @agilezen_mock = double("Backend::AgileZenApi")
+        Backend::AgileZenApi.stub(:new).with().and_return(@agilezen_mock)        
     end
 
 	it "should respond to a root request" do
@@ -19,7 +18,23 @@ describe 'Application' do
 
     it 'should get list of projects' do
         mock_api
+        @agilezen_mock.should_receive(:get_projects).and_return(["project1"])
         get '/projects'
+        last_response.should be_ok        
+    end
+
+    it 'should get stories for a project' do
+        mock_api
+        @agilezen_mock.should_receive(:get_stories_for).with(1).and_return(["story1"])
+        get '/projects/1'
         last_response.should be_ok
+        last_response.body.should include 'story1'
+    end
+
+    it 'should get last moved story for a project' do
+        mock_api        
+        @agilezen_mock.should_receive(:get_last_story_moved_for).with(1).and_return(["last story moved"])
+        get '/projects/1/last_story_moved'
+        last_response.body.should include 'last story moved'
     end
 end
